f9bf45f083b70ca3b712ef64203910ddf7ec30f7
dbakti7 <dbakti1605@gmail.com>
2015-10-31 18:31:56 +0800

    combined with stefan

--- app/src/main/java/boundary/TravelPlanner.java
+++ app/src/main/java/boundary/TravelPlanner.java
@@ -7,15 +7,11 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.DatePicker;
-import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.android.cz2006androidproject.R;
-
-import java.lang.reflect.Method;
 
 import control.TravelPlannerAdapter;
 import entity.Place;
--- app/src/main/java/boundary/accessGovAPI.java
+++ app/src/main/java/boundary/accessGovAPI.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 import javax.net.ssl.HttpsURLConnection;
 
-import Control.jsonParser;
+import control.jsonParser;
 
 /**
  * Created by Stefan on 10/18/2015.
@@ -42,14 +42,7 @@ public class accessGovAPI {
         String myToken = null;
 
         URL obj = new URL("http://www.onemap.sg/API/services.svc/getToken?accessKEY=" + key);
-<<<<<<< HEAD
-        HttpURLConnection con = (HttpURLConnection)obj.openConnection();
-        con.setRequestMethod("GET");
-        con.connect();
-        control.jsonParser temp = new control.jsonParser();
-        myToken = temp.parseToken(con.getInputStream());
 
-=======
         try {
             HttpURLConnection con = (HttpURLConnection) obj.openConnection();
             con.setRequestMethod("GET");
@@ -59,7 +52,7 @@ public class accessGovAPI {
         } catch(Exception e){
             System.out.println("Error in trying to get Token!");
         }
->>>>>>> 28cfa5cf7882899e9f4c586057701577417efd56
+
         return myToken;
     }
 
